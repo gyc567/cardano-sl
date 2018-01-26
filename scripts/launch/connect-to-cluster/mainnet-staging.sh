4453995193e020887b9fae7b13541d163d3fe12e
Ivan Gromakovskii <gie1994@gmail.com>
2018-01-26 15:47:18 +0300

    [CSL-2186] Put all actual log configs into `log-configs/` directory
    This is done to make it easier for us to update those configs when we need.
    For instance, when config syntax changes or we introduce new logger names
    for which we want to customize something.

--- scripts/launch/connect-to-cluster/mainnet-staging.sh
+++ scripts/launch/connect-to-cluster/mainnet-staging.sh
@@ -20,17 +20,17 @@ printf "wallet:
     valency: 1
     fallbacks: 7" > "${TMP_TOPOLOGY_YAML}"
 
-stack exec -- cardano-node                                  \
-    --tlscert ./scripts/tls-files/server.crt                \
-    --tlskey ./scripts/tls-files/server.key                 \
-    --tlsca ./scripts/tls-files/ca.crt                      \
-    --web                                                   \
-    --no-ntp                                                \
-    --topology "${TMP_TOPOLOGY_YAML}"                       \
-    --log-config scripts/log-templates/log-config-qa.yaml   \
-    --logs-prefix "logs/${CLUSTER}"                         \
-    --db-path db-${CLUSTER}                                 \
-    --wallet-db-path wdb-${CLUSTER}                         \
-    --keyfile secret-$CLUSTER.key                           \
-    --configuration-file lib/configuration.yaml             \
+stack exec -- cardano-node                             \
+    --tlscert ./scripts/tls-files/server.crt           \
+    --tlskey ./scripts/tls-files/server.key            \
+    --tlsca ./scripts/tls-files/ca.crt                 \
+    --web                                              \
+    --no-ntp                                           \
+    --topology "${TMP_TOPOLOGY_YAML}"                  \
+    --log-config log-configs/connect-to-cluster.yaml   \
+    --logs-prefix "logs/${CLUSTER}"                    \
+    --db-path db-${CLUSTER}                            \
+    --wallet-db-path wdb-${CLUSTER}                    \
+    --keyfile secret-$CLUSTER.key                      \
+    --configuration-file lib/configuration.yaml        \
     --configuration-key mainnet_dryrun_full
